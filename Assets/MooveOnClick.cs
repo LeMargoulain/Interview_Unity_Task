e9c8443fc4baf47e965ac96efc1fc985351d357a
Anthony <anthony.legourrierec@etudiant.univ-rennes1.fr>
2023-02-02 19:10:20 +0100

    Moove on click script + Orbit fix to allow center object movement

---
+++ Assets/MooveOnClick.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MooveOnClick : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                agent.SetDestination(hit.point);
+
+        }
+    }
+}
--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -7,9 +7,21 @@ public class Orbit : MonoBehaviour
     public GameObject orbitalObject;
     public float speed = 30;
     public Vector3 orbit = new Vector3(0, 1, 0);
+    private Vector3 distance = new Vector3();
 
+
+    void Start()
+    {
+        //Save the initial distance vector between object and his orbital
+        distance = orbitalObject.transform.position - transform.position;
+    }
     void Update()
     {
+        //Set the distance between object and his orbital
+        orbitalObject.transform.position = transform.position + distance;
+        //Rotate the orbital arround the object a set amount
         orbitalObject.transform.RotateAround(transform.position, orbit, speed * Time.deltaTime);
+        //Recompute the distance between the object and his orbital (vector direction changed)
+        distance = orbitalObject.transform.position - transform.position;
     }
 }
